3567d44eab8368968e6cefe2557c9ff24b6f0f57
Antonin <devidalantonin@gmail.com>
2024-11-27 10:06:32 +0100

    grass

--- GrassGeneration/Shaders/compile.bat
+++ GrassGeneration/Shaders/compile.bat
@@ -1,18 +1,21 @@
-if not exist "%2\GrassGeneration\Shaders\" mkdir "%2\GrassGeneration\Shaders
+set sourceDir=%1\GrassGeneration\Shaders
+set outputDir=%2\GrassGeneration\Shaders
 
-echo %2
 
-glslc %1\GrassGeneration\Shaders\shader.vert -o %2\GrassGeneration\Shaders\vert.spv
-glslc %1\GrassGeneration\Shaders\shader.frag -o %2\GrassGeneration\Shaders\frag.spv
+if not exist "%outputDir%" mkdir "%outputDir%"
 
-glslc %1\GrassGeneration\Shaders\color.frag -o %2\GrassGeneration\Shaders\colorfrag.spv
 
-glslc %1\GrassGeneration\Shaders\grass.vert -o %2\GrassGeneration\Shaders\grassvert.spv
-glslc %1\GrassGeneration\Shaders\grass.frag -o %2\GrassGeneration\Shaders\grassfrag.spv
+glslc %sourceDir%\shader.vert -o %outputDir%\vert.spv
+glslc %sourceDir%\shader.frag -o %outputDir%\frag.spv
 
-glslc %1\GrassGeneration\Shaders\grassGen.comp -o %2\GrassGeneration\Shaders\grassGenComp.spv
+glslc %sourceDir%\color.frag -o %outputDir%\colorfrag.spv
 
-glslc %1\GrassGeneration\Shaders\skybox.vert -o %2\GrassGeneration\Shaders\skyboxvert.spv
-glslc %1\GrassGeneration\Shaders\skybox.frag -o %2\GrassGeneration\Shaders\skyboxfrag.spv
+glslc %sourceDir%\grass.vert -o %outputDir%\grassvert.spv
+glslc %sourceDir%\grass.frag -o %outputDir%\grassfrag.spv
+
+glslc %sourceDir%\grassGen.comp -o %outputDir%\grassGenComp.spv
+
+glslc %sourceDir%\skybox.vert -o %outputDir%\skyboxvert.spv
+glslc %sourceDir%\skybox.frag -o %outputDir%\skyboxfrag.spv
 
 pause
